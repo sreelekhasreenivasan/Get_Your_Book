3f962d96cdd2d445657ab06bfa8669cdfa6e1524
Sreelekha Sreenivasan <sreelekhasreenivasan123@gmail.com>
2025-02-18 11:01:36 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./component/Footer";
 import Home from "./pages/Home"
-import AddReview from './pages/Addreview'
 import Viewbook from "./pages/Viewbook";
 import Addbook from "./pages/Addbook";
 import Onebook from "./pages/Onebook";
@@ -18,7 +17,6 @@ const App = () => {
         <Routes>
         <Route path='/login' element={<Login/>}/> 
         <Route path='/' element={<Home/>}/>
-        <Route path='/add-review/:id' element={<AddReview/>}/>
         <Route path='/view-book' element={<Viewbook/>}/>
         <Route path='/add-book' element={<Addbook/>}/>
         <Route path='/update-book/:id' element={<Updatebook/>}/>
